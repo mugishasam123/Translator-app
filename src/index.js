3de0e5a61f11fb7babc85948422d018bccbac0c4
mugishasam123 <mugishasamuel42@gmail.com>
2023-03-15 20:24:12 +0200

    Add server js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv' 
+dotenv.config()
+import express from 'express'
+import { router } from './routes/route.js'
+import { join } from 'path'
+
+const app = express()
+// Set the view engine to ejs
+app.set('view engine', 'ejs')
+// Set the view directory
+app.set('views', join('./src', 'views'))
+// Serve static files from the 'public' folder
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(router)
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000')
+})
+
+export default app
